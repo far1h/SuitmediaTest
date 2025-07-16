f02026da5121256c8c13d99b3b1d9ff9b2010f56
far1h <farihmuhammad45@gmail.com>
2025-07-17 07:31:51 +0800

    Initial Commit

---
+++ SuitmediaTest/SuitmediaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuitmediaTestApp.swift
+//  SuitmediaTest
+//
+//  Created by Farih Muhammad on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SuitmediaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
